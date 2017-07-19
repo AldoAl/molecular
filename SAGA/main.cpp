0f7909a501e0e93750b01b2dca2e873865f0d2e4
AldoAl <arudo.arekushisu@gmail.com>
2017-07-19 15:24:49 -0500

    SAGA

--- SAGA/main.cpp
+++ SAGA/main.cpp
@@ -1,1 +1,16 @@
-main
+#include "poblacion.h"
+#include <time.h>
+
+int main()
+{
+    poblacion P(10,900,0.9,0.3);
+    P.cargar_cadenas("MSA_16507.txt");
+    P.mostrar_poblacion();
+    clock_t t;
+    t = clock();
+    P.ejecucion(500);
+  	t = clock() - t;
+  	cout << "tiempo de demora: " << (float)t/CLOCKS_PER_SEC<< endl;
+    return 0;
+}
+
